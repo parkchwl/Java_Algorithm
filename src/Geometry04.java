76f8136ff8242cbaa931d3ff8bd5f7481e1b0ae0
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-12-21 20:34:30 +0900

    자바 알고리즘 구현
    기하: 직사각형과 삼각형 - 수학은 체육과목 입니다

---
+++ src/Geometry04.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Geometry04{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        System.out.println(n + n*3);
+
+    }
+}
