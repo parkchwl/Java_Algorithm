67c19a67c86052e31fb86d98f7c35410b986661a
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-12-30 21:33:15 +0900

    자바 알고리즘 구현
    브루트 포스 - 분해합

---
+++ src/BruteForce02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BruteForce02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int result = 0;
+
+        for(int i =0; i<N; i++){
+            int number = i;
+            int sum = 0;
+
+            while(number !=0){
+                sum += number % 10;
+                number /= 10;
+            }
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
