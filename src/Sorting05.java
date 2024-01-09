b11f1dcfaf60377de04d78ea361e6077f4528f87
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-01-09 23:24:33 +0900

    자바 알고리즘 구현
    정렬 단계 - 수 정렬하기3

---
+++ src/Sorting05.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sorting05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
