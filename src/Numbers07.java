e71f691b1fcf2eee4a1d055809e5dc7456a71291
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-03-13 23:22:35 +0900

    자바 알고리즘 구현
    약수, 배수와 소수 2 - 베르트랑 공준

---
+++ src/Numbers07.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Numbers07 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			int count = 0;
+			boolean[] arr = new boolean[(2 * n) + 1];
+			
+			if(n == 0) break;
+			
+			arr[0] = arr[1] = true;
+			
+			for(int i = 2; i * i <= 2 * n; i++) {
+				if(!arr[i]) {
+					for(int j = i * i; j <= 2 * n; j += i) {
+						arr[j] = true;
+					}
+				}
+			}
+			
+			for(int i = n + 1; i <= 2 * n; i++) {
+				if(!arr[i]) count++;
+			}
+			System.out.println(count);
+		}
+		
+	}
+
+}
