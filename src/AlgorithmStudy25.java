bacdb51f54062ce09323f76e466f14b68312baa7
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-04-12 09:46:00 +0900

    알고리즘 스터디 과제 25
    백준 1934 - 최소 공배수

---
+++ src/AlgorithmStudy25.java
@@ -0,0 +1,21 @@
+//최소 공배수
+import java.util.*;
+public class AlgorithmStudy25 {
+    static int n;
+    static int gcd(int a, int b) {
+        if(b==0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a * b / gcd(a, b));
+        }
+    }
+}
