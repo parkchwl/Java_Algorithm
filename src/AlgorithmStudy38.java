35cbc0bd7ad247ad0ec683b11627e69f022ffd31
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-05-18 22:13:13 +0900

    알고리즘 스터디 과제 38
    BOJ_2193 이친수

---
+++ src/AlgorithmStudy38.java
@@ -0,0 +1,28 @@
+// BOJ_2193
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AlgorithmStudy38 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long [] d = new long[N+1]; // 0부터 N까지라서 N+1임
+
+        d[0] = 0; 
+        d[1] = 1;
+
+
+
+        for( int i =2; i<=N; i++){
+            d[i] = d[i-1]+ d[i-2];
+        }
+
+        System.out.println(d[N]);
+
+    }
+}
