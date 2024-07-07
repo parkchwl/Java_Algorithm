79c5254e4444314058f3c7e891d3bd0ab37792ac
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-07-07 20:18:24 +0900

    자바 알고리즘 구현
    스택, 큐, 덱 - 균형잡힌 세상

---
+++ src/SQD03.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SQD03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(".")) break;  // 종료 조건
+
+            if (solve(input)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static boolean solve(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
